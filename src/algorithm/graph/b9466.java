582debca6ecf2aab4b127ffb494bea615c18debe
yeji an <anyeji1220@gmail.com>
2021-02-21 23:48:01 +0900

    algorithm 20210221

---
+++ src/algorithm/graph/b9466.java
@@ -0,0 +1,72 @@
+package algorithm.graph;
+// 텀 프로젝트
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class b9466 {
+
+    public static void main(String[] args) throws IOException {
+        solution1();
+    }
+
+    static void solution1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] students = new int[N];
+            boolean[] visited = new boolean[N];
+            int total = N;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                students[i] = Integer.parseInt(st.nextToken()) - 1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) continue;
+
+                int cnt = 0;
+                Deque<Integer> queue = new LinkedList<>();
+                queue.add(i);
+                visited[i] = true;
+
+                loop:
+                while (!queue.isEmpty()) {
+                    int curr = queue.peekLast();
+                    int next = students[curr];
+                    if (!visited[next]) {
+                        // 방문 안했으면
+                        queue.add(next);
+                        visited[next] = true;
+                    }
+                    else{
+                        // 방문했으면
+                        while(!queue.isEmpty()){
+                            // front와 비교
+                            if (queue.peekFirst() == next) {
+                                cnt = queue.size();
+                                break loop;
+                            }
+                            queue.poll();
+                        }// while
+                    }
+                }// while
+
+                if (cnt > 0)
+                    total -= cnt;
+            }// for
+
+            sb.append(total).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/algorithm/graph/b9466_2.java
@@ -0,0 +1,63 @@
+package algorithm.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 텀프로젝트 dfs
+public class b9466_2 {
+    static int total, N;
+    static int[] students;
+    static boolean[] visited, added;
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            total = 0;
+            N = Integer.parseInt(br.readLine());
+            students = new int[N];
+            visited = new boolean[N];
+            added = new boolean[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                students[i] = Integer.parseInt(st.nextToken()) - 1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                dfs(i);
+            }
+
+            sb.append(N - total).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int start) {
+        if (visited[start]) return;
+
+        visited[start] = true;
+        int next = students[start];
+
+        if (!visited[next]) {
+            dfs(next);
+        }
+        if (!added[next]) {
+            for (int s = next; s != start ; s = students[s]) {
+                total++;
+            }
+            total++;
+        }
+
+        added[start] = true;
+    }
+}
