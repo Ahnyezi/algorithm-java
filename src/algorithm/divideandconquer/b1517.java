3a81912afa00bc8f73b58a84e96c47cd6af0a093
yeji an <anyeji1220@gmail.com>
2021-03-09 19:56:54 +0900

    algorithm 20210309

--- src/algorithm/divideandconquer/b1517.java
+++ src/algorithm/divideandconquer/b1517.java
@@ -1,9 +1,11 @@
-package algorithm.bst;
+package algorithm.divideandconquer;
 // 버블 소트 [10:56~]
 // 정렬할 요소 : 1~ 50만
 // 일반적인 버블 소트 : O(N^2) 50만제곱=2500억 => 7%에서 시간초과
 // 머지 소트 O(NlogN) => 반으로 나눠서 값을 비교하므로 최대 50만 * log 2(밑)50만 => 50만 * 19
-// 머지 소트로 정렬하되 앞의 숫자가 뒤의 숫자 보다 큰 경우 카운트를 해주면 된다.
+
+// 버블 정렬의 시간 복잡도로 50만을 처리할 수 없다.
+// 따라서 머지 소트로 정렬하면서 앞의 숫자가 뒤의 숫자보다 큰 경우 카운트를 해주면 된다.
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,7 +13,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class b1517 {
-    static int arr[], res[];
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
@@ -22,52 +23,52 @@ public class b1517 {
     static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        res = new int[N];
+        int[] arr = new int[N];
+        int[] tmp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        mergeSort(0, N - 1);
+        mergeSort(arr, tmp, 0, N - 1);
         System.out.println(cnt);
     }
 
-    static void mergeSort(int low, int high){
-        if (low < high){
-            int mid = (low + high) / 2;
-            mergeSort(low, mid);
-            mergeSort(mid + 1, high);
-            merge(low, mid, high);
+    static void mergeSort(int[] arr, int[] tmp, int start, int end){
+        if (start < end){
+            int mid = (start + end) / 2;
+            mergeSort(arr, tmp, start, mid);
+            mergeSort(arr,tmp,mid + 1, end);
+            merge(arr,tmp, start, mid, end);
         }
     }
 
-    static void merge(int low, int mid, int high){
-        int i = low;
-        int j = mid + 1;
-        int index = low;
+    static void merge(int[] arr, int[] tmp, int start, int mid, int end){
+        for (int i = start; i <= end ; i++) {
+            tmp[i] = arr[i];
+        }
+        int part1 = start;
+        int part2 = mid + 1;
+        int index = start;
 
-        while(i <= mid && j <= high){
-            if (arr[i] <= arr[j]){
-                res[index++] = arr[i++];
+        while(part1 <= mid && part2 <= end){
+            if (arr[part1] <= arr[part2]){
+                tmp[index++] = arr[part1++];
             }else{
-                res[index++] = arr[j++];
-                cnt += (mid + 1 - i);
+                tmp[index++] = arr[part2++];
+                cnt += (mid + 1 - part1);
             }
         }
 
-        while(i <= mid)
-            res[index++] = arr[i++];
 
-        while(j <= high)
-            res[index++] = arr[j++];
-
-        for (int k = 0; k <= high; k++) {
-            arr[k] = res[k];
+        for (int i = 0; i <= mid - part1; i++) {
+            arr[index + i] = tmp[part1 + i];
         }
     }
 
+
+    static int[] arr;
     static void wrongAnswer() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
